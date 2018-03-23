0f97620223a473b7b00f69f507632fa25dfdb736
adrian.sliwka <adrian.sliwka@3code.pl>
2018-03-23 16:10:36 +0100

    Added dropdown list to CommandBar + styles

--- src/ctrl/CommandBar.tsx
+++ src/ctrl/CommandBar.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 import {ICommand} from "../lib/ICommand"
 
 interface IProps {
@@ -23,7 +24,14 @@ export class CommandBar extends React.Component<IProps, any> {
     constructor(props) {
         super(props);
         this.state = {
-            searchedText: ''
+            searchedText: '',
+
+            dropdownHeight: 0,
+            dropdownShowed: false,
+            dropdownVisible: 0,
+            dropdownLayerVisible: "none",
+            dropdownPosition: null,
+            dropdownElementList: null,
         }
     }
 
@@ -51,9 +59,40 @@ export class CommandBar extends React.Component<IProps, any> {
                 <div className="buttons-left">
                     {this.props.items.map((item: ICommand, index: number) => {
                         if (item !== null) {
+                            if (item.subItems !== undefined){
+                                let dropdownHeight = "100px";
+                                let dropdownVisible = "1";
+                                return (
+                                <button className={"bar-dropdown"} key={item.key} onClick={(event) => {
+                                    const elementPosittion = ReactDOM.findDOMNode(event.target).getBoundingClientRect();
+                                    this.setState({
+                                        dropdownElementList: item.subItems,
+                                        dropdownPosition: elementPosittion.left,
+                                    })
+                                    if(this.state.dropdownHeight > 1){
+                                        this.setState({
+                                            dropdownHeight: 0,
+                                            dropdownVisible: 0,
+                                            dropdownLayerVisible: "none",
+                                            dropdownShowed: false,
+                                        })
+                                    } else {
+                                        this.setState({
+                                            dropdownHeight: 500,
+                                            dropdownVisible: 1,
+                                            dropdownLayerVisible: "block",
+                                            dropdownShowed: true,
+                                        })
+                                    }
+                                }}>
+                                    <i className={"ms-Icon ms-Icon--" + item.icon}></i> {item.label} <i className={"ms-Icon ms-Icon--ChevronDown"}></i>
+                                </button>
+                                );
+                            } else {
                             return <a key={item.key} onClick={item.onClick} >
                                 <i className={"ms-Icon ms-Icon--" + item.icon}></i> {item.label}
                             </a>
+                            }
                         }
                         return null;
                     })}
@@ -69,9 +108,22 @@ export class CommandBar extends React.Component<IProps, any> {
                         return null;
                     })}
                 </div>
+                <div className={"dropdown-layer"} style={{display: this.state.dropdownLayerVisible}} onClick={() => {
+                    this.setState({
+                        dropdownLayerVisible: "none",
+                        dropdownHeight: 0,
+                        dropdownVisible: 0,
+                    })
+                }}></div>
+
+                {this.state.dropdownShowed == true &&
+                    <div className={"bar-dropdown-list"} style={{maxHeight: this.state.dropdownHeight, opacity: this.state.dropdownVisible, left: this.state.dropdownPosition - 60}}>
+                        {this.state.dropdownElementList.map((element) => {
+                            return <div className={"bar-dropdown-item"} key={element.key} onClick={element.onClick}><i className={"ms-Icon ms-Icon--" + element.icon}></i><span>{element.label}</span></div>
+                        })}
+                    </div>
+                }
             </div>
-
-
         </div>
     }
 }
