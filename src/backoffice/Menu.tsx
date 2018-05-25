fd7fcce18bd3355afcd6f695f92b9cc6833c4dff
Art Ble <artur.kmera@3code.pl>
2018-05-25 21:44:19 +0200

    Panel menu modyfication

--- src/backoffice/Menu.tsx
+++ src/backoffice/Menu.tsx
@@ -29,9 +29,16 @@ interface IMenuState {
 export class Menu extends React.Component<IMenuProps, IMenuState> {
     constructor(props: IMenuProps) {
         super(props);
+        if (window.localStorage.backofficeMenuOpened === undefined) {
+            window.localStorage.backofficeMenuOpened = "1";
+        }
+
         this.state = {
             currentMenuOpened: -1,
-            expanded: props.mobile,
+            expanded:
+                window.localStorage.backofficeMenuOpened !== undefined
+                    ? window.localStorage.backofficeMenuOpened == "1"
+                    : props.mobile,
         };
     }
 
@@ -56,6 +63,11 @@ export class Menu extends React.Component<IMenuProps, IMenuState> {
         this.props.onMenuElementClick(el);
     }
 
+    public changeExpandState = () => {
+        window.localStorage.backofficeMenuOpened = this.state.expanded ? "0" : "1";
+        this.setState({ expanded: !this.state.expanded });
+    };
+
     public render() {
         let style = {};
         if (this.props.mobile) {
@@ -67,19 +79,39 @@ export class Menu extends React.Component<IMenuProps, IMenuState> {
             };
         }
         return (
-            <div className={"w-menu " + (this.state.expanded ? "w-menu-expanded" : "w-menu-collapsed")} onMouseLeave={this.handleMenuLeave.bind(this)} style={style}>
+            <div
+                className={"w-menu " + (this.state.expanded ? "w-menu-expanded" : "w-menu-collapsed")}
+                onMouseLeave={this.handleMenuLeave.bind(this)}
+                style={style}
+            >
                 {this.props.elements.map((el, index) => (
                     <div className="menu-section" key={index}>
-                        <div className="menu-section-title" onClick={() => this.setState({ currentMenuOpened: index })} onMouseEnter={this.handleTitleEnter.bind(this, index)}>
+                        <div
+                            className="menu-section-title"
+                            onClick={() => this.setState({ currentMenuOpened: index })}
+                            onMouseEnter={this.handleTitleEnter.bind(this, index)}
+                        >
                             <Icon name={el.icon} />
                             <span>{el.title}</span>
                         </div>
-                        <div className={"menu-section-section menu-section-section-" + (index == this.state.currentMenuOpened ? "opened" : "closed")}>
+                        <div
+                            className={
+                                "menu-section-section menu-section-section-" +
+                                (index == this.state.currentMenuOpened ? "opened" : "closed")
+                            }
+                        >
                             {!this.state.expanded && <div className="section-inner-title">{el.title}</div>}
                             {el.elements.map((el) => (
-                                <div key={el.title} className="menu-link" onClick={this.handleElementClick.bind(this, el)}>
+                                <div
+                                    key={el.title}
+                                    className="menu-link"
+                                    onClick={this.handleElementClick.bind(this, el)}
+                                >
                                     {el.title}
-                                    <div onClick={this.handleElementClickOpen.bind(this, el)} className={"menu-link-open-window"}>
+                                    <div
+                                        onClick={this.handleElementClickOpen.bind(this, el)}
+                                        className={"menu-link-open-window"}
+                                    >
                                         <Icon name={"OpenInNewWindow"} />
                                     </div>
                                 </div>
@@ -89,7 +121,7 @@ export class Menu extends React.Component<IMenuProps, IMenuState> {
                 ))}
 
                 {!this.props.mobile && (
-                    <div className="menu-collapse " onClick={() => this.setState({ expanded: !this.state.expanded })}>
+                    <div className="menu-collapse " onClick={this.changeExpandState}>
                         <Icon name={this.state.expanded ? "DoubleChevronLeftMed" : "DoubleChevronLeftMedMirrored"} />
                     </div>
                 )}
