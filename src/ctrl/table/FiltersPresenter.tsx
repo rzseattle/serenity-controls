dad53b2b0403649b63c7d1089c0a26de8f7825d8
Artur Kmera <artur.kmera@3code.pl>
2018-06-04 00:01:53 +0200

    Icons in filters presenter update

--- src/ctrl/table/FiltersPresenter.tsx
+++ src/ctrl/table/FiltersPresenter.tsx
@@ -1,31 +1,36 @@
 import * as React from "react";
 import Icon from "frontend/src/ctrl/Icon";
 
-export default function (props) {
-
+export default function(props) {
     let isVisible = Object.entries(props.order).length > 0 || Object.entries(props.filters).length > 0;
     return (
-        <div className={'w-table-presenter ' + (!isVisible ? 'w-table-presenter-hidden' : '')}>
+        <div className={"w-table-presenter " + (!isVisible ? "w-table-presenter-hidden" : "")}>
             <div className="w-table-presenter-inner">
-                {Object.entries(props.order).map(([field, el], index) =>
+                {Object.entries(props.order).map(([field, el]: any, index) => (
                     <div key={index}>
-                        <div><i className={'fa fa-' + (el.dir == 'asc' ? 'arrow-down' : 'arrow-up')}></i></div>
-                        <div className="caption">{el.caption}</div>
-                        <div className="remove" onClick={(e) => props.orderDelete(field)}><Icon name={"Clear"} /> </div>
+                        <div>
+                            <Icon name={el.dir == "asc" ? "SortDown" : "SortUp"} />
+                        </div>
+                        <div className="caption">{el.caption}</div>-
+                        <div className="remove" onClick={(e) => props.orderDelete(field)}>
+                            <Icon name={"Clear"} />
+                        </div>
                     </div>
-                )}
+                ))}
 
-
-                {Object.entries(props.filters).map(([key, el]) =>
+                {Object.entries(props.filters).map(([key, el]: any) => (
                     <div key={key}>
-                        <div><i className="ms-Icon ms-Icon--Filter"></i></div>
+                        <div>
+                            <i className="ms-Icon ms-Icon--Filter" />
+                        </div>
                         <div className="caption">{el.caption}</div>
-                        <div className="value" dangerouslySetInnerHTML={{__html: el.label}}/>
-                        <div className="remove" onClick={(e) => props.FilterDelete(key)}><Icon name={"Clear"} /></div>
-
+                        <div className="value" dangerouslySetInnerHTML={{ __html: el.label }} />
+                        <div className="remove" onClick={(e) => props.FilterDelete(key)}>
+                            <Icon name={"Clear"} />
+                        </div>
                     </div>
-                )}
+                ))}
             </div>
         </div>
-    )
+    );
 }
