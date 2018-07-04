04be937fbc5bf72a1a3bcca86461cf6d9dbca771
Artur Kmera <artur.kmera@3code.pl>
2018-07-04 15:30:09 +0200

    Files List update

--- src/ctrl/FileLists.tsx
+++ src/ctrl/FileLists.tsx
@@ -1,26 +1,26 @@
 import * as React from "react";
 
-import {arrayMove, SortableContainer, SortableElement, SortableHandle} from 'react-sortable-hoc';
-import Dropzone from 'react-dropzone'
-import {IFieldProps} from "./fields/Interfaces";
-import {Icon} from "frontend/src/ctrl/Icon";
-import {Modal} from "frontend/src/ctrl/Overlays";
-import {CommandBar} from "./CommandBar";
+import { arrayMove, SortableContainer, SortableElement, SortableHandle } from "react-sortable-hoc";
+import Dropzone from "react-dropzone";
+import { IFieldProps } from "./fields/Interfaces";
+import { Icon } from "frontend/src/ctrl/Icon";
+import { Modal } from "frontend/src/ctrl/Overlays";
+import { CommandBar } from "./CommandBar";
 
 let baseUrl = "";
 if (window.location.host.indexOf("esotiq") != -1) {
     baseUrl = "https://static.esotiq.com/";
 }
 
-let parsePath = (path) => {
+const parsePath = (path) => {
     if (path.charAt(0) != "/") {
         path = "/" + path;
     }
     return baseUrl + path;
-}
+};
 
 interface IFile {
-    key: number,
+    key: number;
     name: string;
     size: number;
     description: string;
@@ -29,32 +29,33 @@ interface IFile {
     uploaded: boolean;
     nativeObj?: File;
     path: string;
-
 }
 
-
-const DragHandle = SortableHandle(() => <a className="w-gallery-drag"><Icon name={"SIPMove"}/></a>); //
-
+const DragHandle = SortableHandle(() => (
+    <a className="w-gallery-drag">
+        <Icon name={"SIPMove"} />
+    </a>
+)); //
 
 const Progress = (props) => {
     return (
         <div className="w-gallery-loader">
-            <div style={{width: props.percent + "%"}}></div>
+            <div style={{ width: props.percent + "%" }} />
         </div>
-    )
-}
+    );
+};
 
 const ImageBox = SortableElement((props) => {
-
     const file = props.file;
     let isImage = false;
-    if (file.path.match(/.(jpg|jpeg|png|gif)$/i))
+    if (file.path.match(/.(jpg|jpeg|png|gif)$/i)) {
         isImage = true;
-    if (file.type && file.type.indexOf('image') != -1)
+    }
+    if (file.type && file.type.indexOf("image") != -1) {
         isImage = true;
+    }
 
-
-    let style = props.style || {};
+    const style = props.style || {};
 
     if (!file.uploaded) {
         /*let reader = new FileReader();
@@ -62,237 +63,281 @@ const ImageBox = SortableElement((props) => {
             preview.src = reader.result;
         }, false);
         reader.readAsDataURL(file.nativeObj);*/
-
     }
 
+    return (
+        <div style={style}>
+            <div onClick={() => props.onClick(props._index)} className={"w-image-box"}>
+                <span>
+                    <span />
+                    {file.uploaded ? <img src={parsePath(file.path)} alt="" /> : <Icon name={"Upload"} />}
 
-    return (<div style={style}>
-        <div onClick={() => props.onClick(props._index)} className={"w-image-box"}>
-            <span>
-                <span></span>{file.uploaded ? <img src={parsePath(file.path)} alt=""/> : <Icon name={"Upload"}/>}
-
-                <div className="w-gallery-on-hover">
-                    <a onClick={(e) => {
-                        e.stopPropagation();
-                        props.onDelete(props._index);
-                    }} className="w-gallery-delete"><Icon name={"Clear"}/> </a>
-                    <DragHandle/>
-                </div>
-            </span>
-            <div className="w-gallery-name">{file.name}</div>
+                    <div className="w-gallery-on-hover">
+                        <a
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                props.onDelete(props._index);
+                            }}
+                            className="w-gallery-delete"
+                        >
+                            <Icon name={"Clear"} />{" "}
+                        </a>
+                        <DragHandle />
+                    </div>
+                </span>
+                <div className="w-gallery-name">{file.name}</div>
+            </div>
         </div>
-    </div>)
+    );
 });
 
 const SortableImageList = SortableContainer((props) => {
-
-
     return (
         <div className="w-gallery-list">
-            {props.files && props.files.map((file, index) =>
-                <ImageBox
-                    file={file}
-                    key={file.name}
-                    index={index}
-                    _index={index}
-                    onClick={props.onClick}
-                    onDelete={props.onDelete}
-                    style={props.itemStyle}
-
-                />
-            )}
+            {props.files &&
+                props.files.map((file, index) => (
+                    <ImageBox
+                        file={file}
+                        key={file.name}
+                        index={index}
+                        _index={index}
+                        onClick={props.onClick}
+                        onDelete={props.onDelete}
+                        style={props.itemStyle}
+                    />
+                ))}
         </div>
-    )
+    );
 });
 
-
 export interface IFileList extends IFieldProps {
+    name?: string;
     value: IFile[];
     type?: "gallery" | "filelist";
     buttonTitle?: string;
     maxLength?: number;
     itemStyle?: any;
-
 }
 
 class FileList extends React.Component<IFileList, any> {
-
     public static defaultProps: Partial<IFileList> = {
         type: "filelist",
         maxLength: null,
-        buttonTitle: 'Dodaj',
-        itemStyle: {}
-    }
+        buttonTitle: "Dodaj",
+        itemStyle: {},
+    };
 
     public constructor(props) {
         super(props);
 
         this.state = {
             filesDeleted: [],
-            preview: null
-        }
+            preview: null,
+        };
     }
 
-    public handleFileAdd(addedFiles: (File & { preview: string })[]) {
-        let currFiles = this.props.value ? this.props.value.slice() : [];
+    public handleFileAdd(addedFiles: Array<File & { preview: string }>) {
+        const currFiles = this.props.value ? this.props.value.slice() : [];
         for (let i = 0; i < addedFiles.length; i++) {
             if (this.props.maxLength && i >= this.props.maxLength) {
                 continue;
             }
-            let el = addedFiles[i];
+            const el = addedFiles[i];
             if (this.props.type == "gallery" && !this.isImage(el.name)) {
                 alert(`"${el.name}" to nie plik graficzny`);
                 continue;
             }
 
-
-            let file: IFile = {
+            const file: IFile = {
                 key: null,
                 name: el.name,
                 title: el.name,
                 description: "",
                 path: el.preview,
-                type: 'image',
+                type: "image",
                 uploaded: false,
                 size: el.size,
-                nativeObj: el
-            }
+                nativeObj: el,
+            };
             currFiles.push(file);
         }
 
         this.handleChange(currFiles);
-
-
     }
 
-    componentWillReceiveProps(nextProps) {
-
-        //this.setState({filesDeleted: []});
-        //console.log(nextProps);
+    public componentWillReceiveProps(nextProps) {
+        // this.setState({filesDeleted: []});
+        // console.log(nextProps);
     }
 
-    handleFileClick(index) {
+    public handleFileClick(index) {
         const el = this.props.value[index];
         if (this.isImage(el.path)) {
-            this.setState({preview: el});
+            this.setState({ preview: el });
         } else {
             alert("Brak podglądu do tego rodzaju plików");
         }
     }
 
-    handleMoveFile(moveEvent) {
-        let {oldIndex, newIndex} = moveEvent;
-        let currFiles = this.props.value ? this.props.value.slice() : [];
+    public handleMoveFile(moveEvent) {
+        const { oldIndex, newIndex } = moveEvent;
+        const currFiles = this.props.value ? this.props.value.slice() : [];
         currFiles.splice(newIndex, 0, currFiles.splice(oldIndex, 1)[0]);
         this.handleChange(currFiles);
     }
 
-    handleFileRemove(index) {
-
-        let currFiles = this.props.value ? this.props.value.slice() : [];
-        let deleted = this.state.filesDeleted;
+    public handleFileRemove(index) {
+        const currFiles = this.props.value ? this.props.value.slice() : [];
+        const deleted = this.state.filesDeleted;
         deleted.push(currFiles[index]);
-        this.setState({filesDeleted: deleted});
+        this.setState({ filesDeleted: deleted });
         currFiles.splice(index, 1);
         this.handleChange(currFiles);
-
     }
 
-    handleChange(currFiles) {
+    public handleChange(currFiles) {
         if (this.props.onChange) {
             this.props.onChange({
                 name: this.props.name,
-                type: 'fileList',
+                type: "fileList",
                 value: currFiles,
-                event: null
-            })
+                event: null,
+            });
         }
     }
 
-    formatBytes(bytes) {
-        if (bytes < 1024) return bytes + " Bytes";
-        else if (bytes < 1048576) return (bytes / 1024).toFixed(2) + " KB";
-        else if (bytes < 1073741824) return (bytes / 1048576).toFixed(2) + " MB";
-        else return (bytes / 1073741824).toFixed(2) + " GB";
-    };
-
-    isImage(path) {
-        return (path.match(/.(jpg|jpeg|png|gif)$/i))
+    public formatBytes(bytes) {
+        if (bytes < 1024) {
+            return bytes + " Bytes";
+        } else if (bytes < 1048576) {
+            return (bytes / 1024).toFixed(2) + " KB";
+        } else if (bytes < 1073741824) {
+            return (bytes / 1048576).toFixed(2) + " MB";
+        } else {
+            return (bytes / 1073741824).toFixed(2) + " GB";
+        }
     }
 
-    render() {
-        let {value, type, maxLength} = this.props;
-        let {preview} = this.state;
-        let deleted = this.state.filesDeleted;
+    public isImage(path) {
+        return path.match(/.(jpg|jpeg|png|gif)$/i);
+    }
+
+    public render() {
+        const { value, type, maxLength } = this.props;
+        const { preview } = this.state;
+        const deleted = this.state.filesDeleted;
 
         return (
             <div className="w-file-list">
-                {(!maxLength || (value && value.length < maxLength) || !value) && <Dropzone className="dropzone" activeClassName="w-gallery-add-active" onDrop={this.handleFileAdd.bind(this)}>
-                    <span><Icon name={"Add"}/> {this.props.buttonTitle} </span>
-                </Dropzone>}
+                {(!maxLength || (value && value.length < maxLength) || !value) && (
+                    <Dropzone
+                        className="dropzone"
+                        activeClassName="w-gallery-add-active"
+                        onDrop={this.handleFileAdd.bind(this)}
+                    >
+                        <span>
+                            <Icon name={"Add"} /> {this.props.buttonTitle}{" "}
+                        </span>
+                    </Dropzone>
+                )}
 
                 <div className={" " + (type == "gallery" ? "w-file-list-gallery" : "w-file-list-files")}>
                     {/*{deleted.map((el) => <div>Do usuni�cia: {el.name}</div>)}*/}
-                    {value && Array.isArray(value) && type == "filelist" ? value.map((el, index) => <div className="w-file-list-element" key={el.name}>
-                        <div className="w-file-list-name">
-                            <a onClick={this.handleFileClick.bind(this, index)}><Icon name={this.isImage(el.path) ? "Photo2" : "TextDocument"}/>{el.name}</a>
-                            {!el.uploaded && <div className="w-file-list-upload-info">Plik zostanie załadowany po zapisaniu formularza</div>}
-                        </div>
-                        <div className="w-file-list-size"><a href={el.path} download={true}><Icon name={"Download"}/></a></div>
-                        <div className="w-file-list-size">{this.formatBytes(el.size)}</div>
-                        <div className="w-file-list-remove"><a onClick={this.handleFileRemove.bind(this, index)}><Icon name={"Delete"}/> </a></div>
-                    </div>) : null}
+                    {value && Array.isArray(value) && type == "filelist"
+                        ? value.map((el, index) => (
+                              <div className="w-file-list-element" key={el.name}>
+                                  <div className="w-file-list-name">
+                                      <a onClick={this.handleFileClick.bind(this, index)}>
+                                          <Icon name={this.isImage(el.path) ? "Photo2" : "TextDocument"} />
+                                          {el.name}
+                                      </a>
+                                      {!el.uploaded && (
+                                          <div className="w-file-list-upload-info">
+                                              Plik zostanie załadowany po zapisaniu formularza
+                                          </div>
+                                      )}
+                                  </div>
+                                  <div className="w-file-list-size">
+                                      <a href={el.path} download={true}>
+                                          <Icon name={"Download"} />
+                                      </a>
+                                  </div>
+                                  <div className="w-file-list-size">{this.formatBytes(el.size)}</div>
+                                  <div className="w-file-list-remove">
+                                      <a onClick={this.handleFileRemove.bind(this, index)}>
+                                          <Icon name={"Delete"} />{" "}
+                                      </a>
+                                  </div>
+                              </div>
+                          ))
+                        : null}
                     {/*  <pre>
                         {JSON.stringify(value, null, 2)}
                     </pre>*/}
 
-                    {value && type == "gallery" && <SortableImageList
-                        helperClass={"w-file-list-dragging"}
-                        files={value}
-                        onSortEnd={this.handleMoveFile.bind(this)}
-                        axis={'xy'}
-                        useDragHandle={true}
-                        lockToContainerEdges={true}
-                        onDelete={this.handleFileRemove.bind(this)}
-                        onClick={(index) => {
-                            this.setState({preview: value[index]});
-                        }}
-                        itemStyle={this.props.itemStyle}
-
-                    />}
-
+                    {value &&
+                        type == "gallery" && (
+                            <SortableImageList
+                                helperClass={"w-file-list-dragging"}
+                                files={value}
+                                onSortEnd={this.handleMoveFile.bind(this)}
+                                axis={"xy"}
+                                useDragHandle={true}
+                                lockToContainerEdges={true}
+                                onDelete={this.handleFileRemove.bind(this)}
+                                onClick={(index) => {
+                                    this.setState({ preview: value[index] });
+                                }}
+                                itemStyle={this.props.itemStyle}
+                            />
+                        )}
                 </div>
                 {/*<pre>
                     {JSON.stringify(preview, null, 2)}
                 </pre>*/}
 
-                {preview && <Modal
-                    show={true}
-                    onHide={() => this.setState({preview: null})}
-                    title={preview.name}
-                    showHideLink={true}
-                >
-                    <CommandBar items={[
-                        {
-                            key: "f0", label: "Kopiuj link", icon: "Copy", onClick: () => {
-                                this.clipurl.select();
-                                document.execCommand("Copy");
-                            }
-                        },
-                        {
-                            key: "f1", label: "Otwórz w nowym oknie", icon: "OpenInNewWindow", onClick: () => {
-                                window.open(baseUrl + this.clipurl.value)
-                            }
-                        },
-                    ]}/>
-                    <div style={{opacity: 0}}>
-                        <input className={"form-control"} type="text" value={parsePath( preview.path)} ref={(el) => this.clipurl = el}/>
-                    </div>
-                    <img style={{maxWidth: 800, maxHeight: 600}} src={parsePath( preview.path)}/>
-                </Modal>}
+                {preview && (
+                    <Modal
+                        show={true}
+                        onHide={() => this.setState({ preview: null })}
+                        title={preview.name}
+                        showHideLink={true}
+                    >
+                        <CommandBar
+                            items={[
+                                {
+                                    key: "f0",
+                                    label: "Kopiuj link",
+                                    icon: "Copy",
+                                    onClick: () => {
+                                        this.clipurl.select();
+                                        document.execCommand("Copy");
+                                    },
+                                },
+                                {
+                                    key: "f1",
+                                    label: "Otwórz w nowym oknie",
+                                    icon: "OpenInNewWindow",
+                                    onClick: () => {
+                                        window.open(baseUrl + this.clipurl.value);
+                                    },
+                                },
+                            ]}
+                        />
+                        <div style={{ opacity: 0 }}>
+                            <input
+                                className={"form-control"}
+                                type="text"
+                                value={parsePath(preview.path)}
+                                ref={(el) => (this.clipurl = el)}
+                            />
+                        </div>
+                        <img style={{ maxWidth: 800, maxHeight: 600 }} src={parsePath(preview.path)} />
+                    </Modal>
+                )}
             </div>
-        )
+        );
     }
 }
 
-export {FileList};
+export { FileList };
