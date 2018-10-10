7c2e4de87714dff2ec233f5b6fa1ab3d6334e1d4
Art Ble <artur.kmera@3code.pl>
2018-10-10 08:21:05 +0200

    Copy files to right folders

--- sass-copy.js
+++ sass-copy.js
@@ -6,3 +6,12 @@ glob("src/**/*.sass", {}, function(er, files) {
         fs.copyFile(file, file.replace("src/", "lib/"), () => {});
     }
 });
+
+glob("lib/src/**/*.*", {}, function(er, files) {
+    for (let file of files) {
+        fs.renameSync(file, file.replace("lib/src/", "lib/"), () => {});
+    }
+
+    var rimraf = require("rimraf");
+    rimraf("lib/src", () => {});
+});
