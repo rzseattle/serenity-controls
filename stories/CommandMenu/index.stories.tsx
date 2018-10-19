1134f26b38ea5acf6b9b1cf68cfe05ee9a548a1b
Artur Kmera <artur.kmera@3code.pl>
2018-10-19 13:02:00 +0200

    Removed command bar import

--- stories/CommandMenu/index.stories.tsx
+++ stories/CommandMenu/index.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
-import { CommandBar } from "../../src/CommandBar";
 import { CommandMenu } from "../../src/CommandMenu";
 import "./CommandMenu.stories.sass";
 
