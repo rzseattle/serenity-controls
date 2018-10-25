cb751d623838f0c2e684c7db96ea97598bf25ebb
Artur Kmera <artur.kmera@3code.pl>
2018-10-25 17:25:17 +0200

    Changed config to inerface

--- src/backoffice/Config.ts
+++ src/backoffice/Config.ts
@@ -1,34 +1,57 @@
 import { deepExtend } from "../lib/JSONTools";
+import { IFileViewerProps } from "../FileListField";
 
-const config: any = {
+import { ImageViewer } from "../viewers/ImageViewer";
+import { PDFViewer } from "../viewers/PDFViewer";
+
+interface IConfig {
+    translations: {
+        defaultLanguage: string;
+        languages: string[];
+        langChanged: (langCode: string) => any;
+    };
+    files: {
+        viewerRegistry: Array<{ filter: RegExp; viewer: React.ComponentType<IFileViewerProps> }>;
+        transformFilePath: (path: string) => string;
+    };
+}
+
+const config: IConfig = {
     translations: {
         defaultLanguage: "pl",
         languages: [],
-        backendLangChanged: "/admin/changeLang/{{lng}}",
+        langChanged: (langCode) => {
+            // Comm._get("/admin/changeLang/" + langCode);
+            alert("provide lang change action");
+        },
+    },
+    files: {
+        transformFilePath: (path: string) => {
+            let baseUrl = "";
+            if (window.location.host.indexOf("esotiq") != -1) {
+                baseUrl = "https://static.esotiq.com/";
+            }
+
+            if (path.charAt(0) != "/") {
+                path = "/" + path;
+            }
+            return baseUrl + path;
+        },
+        viewerRegistry: [
+            {
+                filter: /.(jpg|jpeg|png|gif)$/i,
+                viewer: ImageViewer,
+            },
+            {
+                filter: /.(pdf)$/i,
+                viewer: PDFViewer,
+            },
+        ],
     },
 };
 
-export const configGet = (path: string, fallbackVal: any = null) => {
-    const result = fetchFromObject(config, path);
-    return result != undefined ? result : fallbackVal;
-};
+export const configGetAll = (): IConfig => config;
 
 export const configSet = (newConfig: any) => {
     deepExtend(config, newConfig);
 };
-
-function fetchFromObject(obj: any, key: string): any {
-    key = key !== undefined ? key : "";
-
-    if (typeof obj === "undefined") {
-        return undefined;
-    }
-
-    const index = key.indexOf(".");
-
-    if (index > -1) {
-        return fetchFromObject(obj[key.substring(0, index)], key.substr(index + 1));
-    }
-
-    return obj[key];
-}
