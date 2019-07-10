283b01147efe8db04030edeb4c97943637e5cb50
Artur Kmera <artur.kmera@as-pl.com>
2019-07-10 13:04:56 +0200

    c

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -181,7 +181,7 @@ export class Table extends React.Component<ITableProps, ITableState> {
         return this.state.data;
     }
 
-    public componentDidUpdate(prevProps: ITableProps, prevState1243567) {
+    public componentDidUpdate(prevProps: ITableProps, prevState12436) {
 
         const state = this.state;
         if (this.props.rememberState) {
