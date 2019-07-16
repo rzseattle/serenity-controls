b0457dfd6ae1888b1f299b905d1d220dc68e66d8
Artur Kmera <artur.kmera@as-pl.com>
2019-07-16 13:47:29 +0200

    Added more download examples

--- docs/stories/Download/index.stories.tsx
+++ docs/stories/Download/index.stories.tsx
@@ -7,14 +7,16 @@ import { Panel } from "../../../src/Panel";
 import { TabPane, Tabs } from "../../../src/Tabs";
 import { Placeholder } from "../../../src/Placeholder";
 import { Navbar } from "../../../src/Navbar";
-import { download, DownloadButton } from "../../../src/Downloader";
+import { download, DownloadButton, downloadString } from "../../../src/Downloader";
 
 storiesOf("Download", module)
     .add("Helper", () => (
         <Panel>
             <a
                 onClick={() =>
-                    download("https://as-pl.com/resources/img/logo_common.png").then((result) => alert("Downloaded file: " + result.fileName))
+                    download("https://as-pl.com/resources/img/logo_common.png").then((result) =>
+                        alert("Downloaded file: " + result.fileName),
+                    )
                 }
             >
                 Download
@@ -23,9 +25,32 @@ storiesOf("Download", module)
     ))
     .add("Button", () => (
         <Panel>
+            <h4>Icon</h4>
+
+            <DownloadButton
+                url={"https://as-pl.com/resources/img/logo_common.png"}
+                onFinish={(result) => alert("Downloaded file: " + result.fileName)}
+                mode="icon"
+            />
+
+            <h4>Link</h4>
+
             <DownloadButton
                 url={"https://as-pl.com/resources/img/logo_common.png"}
                 onFinish={(result) => alert("Downloaded file: " + result.fileName)}
             />
+
+            <h4>Button</h4>
+
+            <DownloadButton
+                url={"https://as-pl.com/resources/img/logo_common.png"}
+                onFinish={(result) => alert("Downloaded file: " + result.fileName)}
+                mode="button"
+            />
+        </Panel>
+    ))
+    .add("Download string as file", () => (
+        <Panel>
+            <a onClick={() => downloadString("test_file.txt", "This is test data")}>Download</a>
         </Panel>
     ));
