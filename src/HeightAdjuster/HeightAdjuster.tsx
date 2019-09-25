fc2d505e07cbab0a312e706046c14b081079906b
Artur Kmera <artur.kmera@as-pl.com>
2019-09-25 12:53:50 +0200

    Added adjust to w-panel-body (ugly, have to change in future )

--- src/HeightAdjuster/HeightAdjuster.tsx
+++ src/HeightAdjuster/HeightAdjuster.tsx
@@ -11,12 +11,15 @@ const HeightAdjuster = React.memo((props: IIconProps) => {
 
     const [height, setHeight] = useState(-1);
 
-    const originalWindowHeight = document.documentElement.scrollHeight;
+    let el: Element = document.documentElement;
+    el = document.getElementsByClassName("w-panel-body")[0];
+    const originalWindowHeight = el.scrollHeight;
 
     useEffect(() => {
-        if (props.parent === undefined) {
-            if (originalWindowHeight < document.documentElement.scrollHeight) {
-                setHeight(height - (document.documentElement.scrollHeight - originalWindowHeight));
+        if (props.parent === undefined && height != -1) {
+            //w-panel-body
+            if (originalWindowHeight < el.scrollHeight) {
+                setHeight(height - (el.scrollHeight - originalWindowHeight));
             }
         }
     }, [height]);
@@ -41,7 +44,7 @@ const HeightAdjuster = React.memo((props: IIconProps) => {
     }, []);
 
     return (
-        <div style={{ height: height == -1 ? "auto" : height + "px" }} ref={divRef}>
+        <div style={{ height: height == -1 ? "100px" : height + "px" }} ref={divRef}>
             {typeof props.children == "function" ? props.children(height) : props.children}
         </div>
     );
