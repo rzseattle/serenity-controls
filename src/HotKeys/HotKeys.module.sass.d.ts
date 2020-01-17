1f94d5cb5e9ff0cfc427e92e7b736271fa5eb812
Artur Kmera <artur.kmera@as-pl.com>
2020-01-17 11:09:02 +0100

    Updates to docker deploy version

--- src/HotKeys/HotKeys.module.sass.d.ts
+++
@@ -1,6 +0,0 @@
-export interface IHotKeysModuleSass {
-    div: string;
-}
-
-export const locals: IHotKeysModuleSass;
-export default locals;
--- src/Table/cells/editable/EditableTextCell.module.sass.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IEditableTextCellModuleSass {
-    accept: string;
-    cancel: string;
-    container: string;
-    errors: string;
-}
-
-export const locals: IEditableTextCellModuleSass;
-export default locals;
