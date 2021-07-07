65eb6012329ac72c2133af8ae92480bef6f80df9
Artur Kmera <artur.kmera@3code.pl>
2021-07-07 13:25:46 +0200

    Changed display

--- src/LoadingIndicator/LoadingIndicator.tsx
+++ src/LoadingIndicator/LoadingIndicator.tsx
@@ -6,29 +6,20 @@ interface ILoadingIndicatorProps {
     size?: number;
 }
 
-class LoadingIndicator extends React.Component<ILoadingIndicatorProps, any> {
-    public static defaultProps: Partial<ILoadingIndicatorProps> = {
-        size: 1,
-        text: null,
-    };
-
-    public render() {
-        const { size, text } = this.props;
-
-        return (
-            <div className="w-loading-indicator">
-                <div>
-                    <span className={"size" + size}>
-                        <i />
-                        <i />
-                        <i />
-                        <i />
-                    </span>
-                    {text && <div className="w-loading-indicator-text">{text}</div>}
-                </div>
+const LoadingIndicator = ({ size = 1, text = null }: ILoadingIndicatorProps) => {
+    return (
+        <div className="w-loading-indicator">
+            <div>
+                <span className={"size" + size}>
+                    <i />
+                    <i />
+                    <i />
+                    <i />
+                </span>
+                {text && <div className="w-loading-indicator-text">{text}</div>}
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export { LoadingIndicator };
