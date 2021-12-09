347948bcb72223cbabcb4a811c06fa5970281d3b
Artur Kmera <artur.kmera@3code.pl>
2021-12-10 00:00:01 +0100

    Updated DataGrid

--- src/DataGrid/parts/GridHead.tsx
+++ src/DataGrid/parts/GridHead.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { IGridColumnData } from "../interfaces/IGridColumnData";
-
+import styles from "./GridHead.module.sass"
 const GridHead = <T,>({ columns }: { columns: IGridColumnData<T>[] }) => {
     return (
         <>
             {columns.map((column) => {
-                return <div key={column.field}>{column.header.caption}</div>;
+                return (
+                    <div key={column.field} className={styles.headerCell}>
+                        {column.header.caption}
+                    </div>
+                );
             })}
         </>
     );
