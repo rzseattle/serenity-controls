13550d0e6340db776fc6b0256d87ac8cbd15b9dd
Artur Kmera <artur.kmera@3code.pl>
2022-02-03 21:28:58 +0100

    Forms change

--- src/Form2/Form2.tsx
+++ src/Form2/Form2.tsx
@@ -1,58 +1,91 @@
-import { Formik, Form, Field, ErrorMessage, useFormikContext } from "formik";
-import { FieldProps } from "formik/dist/Field";
-import { useRef } from "react";
-const Form2 = ({ children }: { children: React.ReactNode }) => {
-    const ref = useRef(null);
+import { FormProvider, useController, useForm, useFormContext } from "react-hook-form";
+import { FieldPath } from "react-hook-form/dist/types/path";
+
+const Form2 = () => {
+    return (
+        <>
+            <Form2HookForm />
+        </>
+    );
+};
+
+interface IFormValues {
+    a: string;
+    b: string;
+}
+
+const Form2HookForm = () => {
+    const methods = useForm<IFormValues>({
+        defaultValues: {
+            a: null,
+            b: null,
+        },
+    });
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+        setValue,
+        setError,
+    } = methods;
+    const onSubmit = (data: IFormValues) => console.log(data);
+
+    console.log("rendering form");
+
+    return (
+        <FormProvider {...methods}>
+            <button
+                onClick={() => {
+                    setError("a", { type: "required" });
+                }}
+            >
+                error !
+            </button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <FormBody<IFormValues>>
+                    <input type="text" {...register("a")} />
+                    <Text name={"z"} />
+                    <Text name={"a.b.0"} />
+                    <input type="submit" />
+                </FormBody>
+            </form>
+        </FormProvider>
+    );
+};
+
+const FormBody = <T,>({ children }: { children: React.ReactElement<T>[] }) => {
+    return children;
+};
+
+const CommonInput = ({ children }: { children: React.ReactElement }) => {
     return (
         <div>
-            <h1>Any place in your app!</h1>
-            <button onClick={() => {
-                alert(JSON.stringify(ref.current.values, null, 2));
-            }}>ok</button>
-            <Formik
-                innerRef={ref}
-                initialValues={{ email: "", password: "", sub: { test: "aaaa" } }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
-                }}
-
-            >
-                <Form>
-                    <Field type="text" name="email" />
-                    <ErrorMessage name="email" component="div" />
-                    <Field type="text" name="password" />
-                    <ErrorMessage name="password" component="div" />
-                    <button type="submit">Submit</button>
-                    <Text />
-                </Form>
-            </Formik>
+            <label>This is label</label>
+            {children}
         </div>
     );
 };
 
-const Text = () => {
-    const formik = useFormikContext();
+const Text = <Value,>({ name }: { name: FieldPath<Value> }) => {
+    const { register, control, setValue } = useFormContext(); // retrieve all hook methods
+
+    // field: { onChange, onBlur, name, value, ref },
+    // fieldState: { invalid, isTouched, isDirty },
+    // formState: { touchedFields, dirtyFields }
+
+    const { field, fieldState, formState } = useController({
+        name,
+        control,
+        rules: { required: true },
+        defaultValue: "",
+    });
+
+    console.log("rendering input");
 
     return (
-        <Field name={"sub.test"}>
-            {({ field, form, meta }: FieldProps) => {
-                return (
-                    <input
-                        type="text"
-                        value={field.value}
-                        name={field.name}
-
-                        readOnly={meta.touched}
-                        onChange={(e) => {
-                            formik.setFieldValue(field.name, e.target.value);
-                        }}
-                    />
-                );
-            }}
-        </Field>
+        <CommonInput>
+            <input value={field.value} onChange={(e) => setValue(name, e.target.value)} />
+        </CommonInput>
     );
 };
 
