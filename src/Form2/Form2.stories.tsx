26e8d2b380f106a9157fe55eaf11439675796826
Artur Kmera <artur.kmera@as-pl.com>
2022-02-04 18:18:20 +0100

    Form play

--- src/Form2/Form2.stories.tsx
+++ src/Form2/Form2.stories.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import Form2 from "./Form2";
+import Form2, { useSuperForm } from "./Form2";
+import { Controller, FormProvider, useForm } from "react-hook-form";
+import Text from "./Inputs/Text/Text";
 
 export default {
     title: "Form/Form 1",
@@ -9,9 +11,48 @@ export default {
     argTypes: {},
 } as ComponentMeta<typeof Form2>;
 
+interface IUser {
+    name: string;
+    age: number;
+    email: string;
+    canEdit: boolean;
+}
+
+const Example = () => {
+    const { field,  setReadonly,register, ...form } = useSuperForm<IUser>({
+        defaultValues: {
+            name: "Super name",
+            age: 11,
+            email: "email@op.com",
+            canEdit: true,
+        },
+    });
+
+    //console.log(register("name"));
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    //form.
+                    alert(JSON.stringify(form.getValues()));
+                    setReadonly(true)
+                }}
+            >
+                submit
+            </button>
+
+            <div>
+                <input {...register("email")} />
+                <input {...register("name")} />
+                <Text label={"Wartość a"} {...field("email")} />
+            </div>
+        </div>
+    );
+};
+
 //👇 We create a “template” of how args map to rendering
 const Template: ComponentStory<typeof Form2> = (args) => {
-    return <Form2 {...args} />;
+    return <Example />;
 };
 
 export const StoryBasic = Template.bind({});
--- src/Form2/Form2.tsx
+++ src/Form2/Form2.tsx
@@ -1,91 +1,97 @@
-import { FormProvider, useController, useForm, useFormContext } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
+import { Row } from "../Row";
+import Text from "./Inputs/Text/Text";
+import { UseFormProps, UseFormReturn } from "react-hook-form/dist/types";
+import { useState } from "react";
 import { FieldPath } from "react-hook-form/dist/types/path";
-
-const Form2 = () => {
-    return (
-        <>
-            <Form2HookForm />
-        </>
-    );
-};
+import { value } from "../DataGrid/parts/Addons/GridConditionsPresenter/GridConditionsPresenter.module.sass";
+import { Control } from "react-hook-form/dist/types/form";
 
 interface IFormValues {
     a: string;
     b: string;
 }
 
-const Form2HookForm = () => {
-    const methods = useForm<IFormValues>({
-        defaultValues: {
-            a: null,
-            b: null,
-        },
-    });
-    const {
-        handleSubmit,
-        register,
-        formState: { errors },
-        setValue,
-        setError,
-    } = methods;
-    const onSubmit = (data: IFormValues) => console.log(data);
+interface IFieldProps<T, TContext extends object = object> {
+    control: Control<T, TContext>;
+    name: any;
+}
 
+interface ISuperForm<T, TContext extends object = object> extends UseFormReturn<T, TContext> {
+    setFormErrors: (errors: string[]) => any;
+    setReadonly: (readonly: boolean) => any;
+    field: (name: FieldPath<T>) => IFieldProps<T>;
+}
+export const useSuperForm = <T,>(props?: UseFormProps<T, any>): ISuperForm<T> => {
+    const form = useForm<T>(props);
+    const setFormErrors = useState<string[]>([]);
+    const setReadOnly = useState<boolean>(false);
+
+    return {
+        ...form,
+        setFormErrors: (errors) => {
+            setFormErrors[1](errors);
+        },
+        setReadonly: (isReadOonly) => {
+            setReadOnly[1](isReadOonly);
+        },
+        field: (name) => {
+            return {
+                control: form.control,
+                name,
+                readonly: setReadOnly[0]
+            };
+        },
+    };
+};
+
+export interface IForm2Props<T, TContext extends object = object> {
+    form: ISuperForm<T, TContext>;
+}
+
+const Form2 = <T,>(props: IForm2Props<T>) => {
+    const onSubmit = (data: T) => {
+        console.log(data);
+    };
+
+    //const [errors, setErrors] = useState<FormError>([])
     console.log("rendering form");
+    console.log(errors[null]);
 
     return (
-        <FormProvider {...methods}>
+        <FormProvider {...props.form}>
             <button
                 onClick={() => {
-                    setError("a", { type: "required" });
+                    props.form.setError("a", {
+                        types: {
+                            0: "To jest error",
+                            1: "To jest cos innego",
+                        },
+                        message: "",
+                    });
+
+                    props.form.setError(null, {
+                        types: {
+                            0: "To jest error",
+                            1: "To jest cos innego",
+                        },
+                        message: "",
+                    });
                 }}
             >
                 error !
             </button>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <FormBody<IFormValues>>
-                    <input type="text" {...register("a")} />
-                    <Text name={"z"} />
-                    <Text name={"a.b.0"} />
-                    <input type="submit" />
-                </FormBody>
+            <br />
+            <form onSubmit={props.form.handleSubmit(onSubmit)}>
+                <div style={{ width: 400 }}>
+                    <Row>
+                        <Text<IFormValues> label={"Wartość b"} name={"b"} />
+                        <Text<IFormValues> label={"Wartość a"} name={"a"} />
+                    </Row>
+                </div>
+                <input type="submit" />
             </form>
         </FormProvider>
-    );
-};
-
-const FormBody = <T,>({ children }: { children: React.ReactElement<T>[] }) => {
-    return children;
-};
-
-const CommonInput = ({ children }: { children: React.ReactElement }) => {
-    return (
-        <div>
-            <label>This is label</label>
-            {children}
-        </div>
-    );
-};
-
-const Text = <Value,>({ name }: { name: FieldPath<Value> }) => {
-    const { register, control, setValue } = useFormContext(); // retrieve all hook methods
-
-    // field: { onChange, onBlur, name, value, ref },
-    // fieldState: { invalid, isTouched, isDirty },
-    // formState: { touchedFields, dirtyFields }
-
-    const { field, fieldState, formState } = useController({
-        name,
-        control,
-        rules: { required: true },
-        defaultValue: "",
-    });
-
-    console.log("rendering input");
-
-    return (
-        <CommonInput>
-            <input value={field.value} onChange={(e) => setValue(name, e.target.value)} />
-        </CommonInput>
     );
 };
 
