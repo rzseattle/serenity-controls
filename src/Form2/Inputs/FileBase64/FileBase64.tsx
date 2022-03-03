90102bd5bdc255e7c7b5e0db2c32e39b80ed1c54
Artur Kmera <artur.kmera@3code.pl>
2022-03-03 23:57:33 +0100

    Added encoded file field

---
+++ src/Form2/Inputs/FileBase64/FileBase64.tsx
@@ -0,0 +1,50 @@
+import CommonInput, { ICommonInputProps } from "../CommonInput/CommonInput";
+import { Control, useController } from "react-hook-form";
+
+const toBase64 = (file: File): Promise<string> =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result + "");
+        reader.onerror = (error) => reject(error);
+    });
+
+export interface IFileBase64Props extends ICommonInputProps {
+    name?: string;
+    value?: string;
+    readonly?: boolean;
+    control: Control<any, any>;
+}
+
+const FileBase64 = (props: IFileBase64Props) => {
+    const control = useController({ name: props.name, control: props.control });
+
+    return (
+        <CommonInput label={props.label} fieldState={control.fieldState}>
+            <input
+                type={"file"}
+                onChange={async (e) => {
+                    const file = e.currentTarget.files[0];
+                    const content = (await toBase64(file)).split(",")[1];
+                    control.field.onChange({
+                        target: {
+                            value: [
+                                {
+                                    name: file.name,
+                                    title: file.name,
+                                    size: file.size,
+                                    mime: file.type,
+                                    key: "",
+                                    uploaded: false,
+                                    content,
+                                },
+                            ],
+                        },
+                    });
+                }}
+            />
+        </CommonInput>
+    );
+};
+
+export default FileBase64;
