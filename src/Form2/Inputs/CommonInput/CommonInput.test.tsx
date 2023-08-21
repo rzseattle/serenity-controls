ca4fa23a7a4d4d8d676e35e6952cb153d1524486
Artur Kmera <artur.kmera@3code.pl>
2023-08-21 17:19:45 +0200

    Lib bump

--- src/Form2/Inputs/CommonInput/CommonInput.test.tsx
+++ src/Form2/Inputs/CommonInput/CommonInput.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, screen, render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import { act, renderHook } from "@testing-library/react-hooks";
 import "@testing-library/jest-dom/extend-expect";
